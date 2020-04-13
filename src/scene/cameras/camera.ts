c0e9620f777f6822b79552a1aa50d7fa30c3db72
Arthur3DLHC <462747437@qq.com>
2020-04-13 16:31:36 +0800

    camera properties

--- src/scene/cameras/camera.ts
+++ src/scene/cameras/camera.ts
@@ -1,5 +1,11 @@
 import { Object3D } from "../object3D.js";
+import { vec4 } from "gl-matrix";
 
 export class Camera extends Object3D {
     // todo: skybox? or put skybox in scene?
+
+    /**
+     * if null. use full window.
+     */
+    public viewport: vec4 | null = null;
 }
--- src/scene/cameras/perspectiveCamera.ts
+++ src/scene/cameras/perspectiveCamera.ts
@@ -3,5 +3,8 @@ import { Camera } from "./camera.js";
 export class PerspectiveCamera extends Camera {
     public fov: number = 60;
     public near: number = 0.1;
-    public far: number = 1000;
+    public far: number = 2000;
+    public focus: number = 10;
+    public aspect: number = 1;
+
 }
