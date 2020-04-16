65528f309a6ac0701cb79b265b6ca2fdb51d76bc
Arthur3DLHC <462747437@qq.com>
2020-04-16 11:37:48 +0800

    decal properties

--- src/scene/decal.ts
+++ src/scene/decal.ts
@@ -1,7 +1,17 @@
 import { Object3D } from "./object3D.js";
+import { Texture } from "../WebGLResources/texture.js";
+import { vec4 } from "gl-matrix";
 
 export class Decal extends Object3D {
-    // todo: pose and location
+    public constructor() {
+        super();
+        this.texture = null;
+        this.atlasRect = vec4.fromValues(0, 0, 128, 128);
+    }
+    // the pose and location is defined by transform matrix.
 
-    // todo: decal texture
+    public texture: Texture | null;
+    public atlasRect: vec4;
+
+    // todo: blend mode?
 }
