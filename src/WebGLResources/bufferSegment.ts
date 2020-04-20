0f0a4810504f398fc64b8b93cb120fa5e489ce6c
Arthur3DLHC <462747437@qq.com>
2020-04-20 19:01:45 +0800

    build uniform buffer

---
+++ src/WebGLResources/bufferSegment.ts
@@ -0,0 +1,9 @@
+export class BufferSegment {
+    public constructor() {
+        this.start = 0;
+        this.count = 0;
+    }
+
+    public start: number;
+    public count: number;
+}
--- src/WebGLResources/uniformBuffer.ts
+++ src/WebGLResources/uniformBuffer.ts
@@ -1,17 +1,27 @@
 import { vec2, vec3, vec4, mat2, mat3, mat4 } from "gl-matrix";
+import { BufferSegment } from "./bufferSegment.js";
+import { GLDevice } from "./glDevice.js";
 
 export class UniformBuffer {
-    public constructor() {
+    public constructor(dynamic: boolean = true) {
         this.bufferData = null;
         this.bufferGL = null;
+        this._uniforms = {};
+        this._dynamic = dynamic;
     }
     public bufferData: Float32Array | null;
     public bufferGL: WebGLBuffer | null;
 
+    private _dynamic: boolean;
+    private _uniforms: {[key: string]: BufferSegment};
+
     // todo: method for add/set/remove named uniform variables
     public addUniform(name: string, numFloats: number) {
+        if (this._uniforms[name] !== undefined) {
+            throw new Error("Uniform" + name + "already exist.");
+        }
+        // 注意：需要保证布局匹配添加顺序
         throw new Error("Not implemented");
-        
     }
 
     public addFloat(name: string, val: number) {
@@ -71,9 +81,19 @@ export class UniformBuffer {
     }
 
     public build() {
-        // build bufferData and offsets according to uniform variables
-        // 注意按 float4 对齐
-        throw new Error("Not implemented");
+        if (!this.bufferData) {
+            return;
+        }
+
+        // 用当前 buffer 数据创建glUniformBuffer对象
+        this.bufferGL = GLDevice.gl.createBuffer();
+        GLDevice.gl.bindBuffer(GLDevice.gl.UNIFORM_BUFFER, this.bufferGL);
+        if (this._dynamic) {
+            GLDevice.gl.bufferData(GLDevice.gl.UNIFORM_BUFFER, this.bufferData, GLDevice.gl.DYNAMIC_DRAW);
+        } else {
+            GLDevice.gl.bufferData(GLDevice.gl.UNIFORM_BUFFER, this.bufferData, GLDevice.gl.STATIC_DRAW);
+        }
+        GLDevice.gl.bindBuffer(GLDevice.gl.UNIFORM_BUFFER, null);
     }
 
     public update() {
