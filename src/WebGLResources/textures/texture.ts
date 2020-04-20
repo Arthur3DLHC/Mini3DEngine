d275f1bf06eca68f270a9978174680c73cbfc37c
Arthur3DLHC <462747437@qq.com>
2020-04-20 16:23:40 +0800

    texture source image

--- src/WebGLResources/textures/texture.ts
+++ src/WebGLResources/textures/texture.ts
@@ -1,4 +1,5 @@
-import { GLDevice } from "../glDevice";
+import { GLDevice } from "../glDevice.js";
+import { DataArray } from "../dataArray.js";
 
 /**
  * Base class for textures
@@ -6,12 +7,16 @@ import { GLDevice } from "../glDevice";
 export class Texture {
     public constructor() {
         this.glTexture = null;
+        this.image = null;
         this.width = 0;
         this.height = 0;
         this.depth = 0;
         this.format = GLDevice.gl.RGBA;
         this.componentType = GLDevice.gl.UNSIGNED_BYTE;
     }
+    // todo: 源图片或数据？是否放在基类中？
+    // 是否使用一个多类型成员？
+    public image: HTMLImageElement | HTMLCanvasElement | ImageData | DataArray | null;
 
     public glTexture: WebGLTexture | null;
 
