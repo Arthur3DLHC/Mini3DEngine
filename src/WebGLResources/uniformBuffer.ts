987ee23db9ad8569b86228a8a5726af6c2b9c748
Arthur3DLHC <462747437@qq.com>
2020-04-21 11:14:03 +0800

    align uniforms

--- src/WebGLResources/uniformBuffer.ts
+++ src/WebGLResources/uniformBuffer.ts
@@ -4,16 +4,20 @@ import { GLDevice } from "./glDevice.js";
 
 export class UniformBuffer {
     public constructor(dynamic: boolean = true) {
-        this.bufferData = null;
+        this._data = [];
+        this._bufferData = null;
         this.bufferGL = null;
         this._uniforms = {};
         this._dynamic = dynamic;
+        this._currStartIdx = 0;
     }
-    public bufferData: Float32Array | null;
     public bufferGL: WebGLBuffer | null;
 
     private _dynamic: boolean;
     private _uniforms: {[key: string]: BufferSegment};
+    private _currStartIdx: number;
+    private _bufferData: Float32Array | null;
+    private _data: number[];
 
     // todo: method for add/set/remove named uniform variables
     public addUniform(name: string, numFloats: number) {
@@ -21,7 +25,22 @@ export class UniformBuffer {
             throw new Error("Uniform" + name + "already exist.");
         }
         // 注意：需要保证布局匹配添加顺序
-        throw new Error("Not implemented");
+        // 注意：std140 对齐
+        // 注意是先对齐；对齐的意义，例如：
+        // 如果已经往Buffer里放了一个float3:          x y z
+        // 那么下一个如果放 float，是可以继续的：      x y z x
+        // 但如果要放vec2，则放不下了，需要对齐：      x y z 0 | x y
+        // 所以是先检查对齐，再添加。
+        // 另外目前只使用了浮点数的uniform，没考虑其它类型。
+        this.alignUniform(numFloats);
+        const unifom: BufferSegment = new BufferSegment();
+        unifom.start = this._currStartIdx;
+        unifom.count = numFloats;
+        this._uniforms[name] = unifom;
+        this._currStartIdx += numFloats;
+        for (let i = 0; i < numFloats; i++) {
+            this._data.push(0);            
+        }
     }
 
     public addFloat(name: string, val: number) {
@@ -81,7 +100,7 @@ export class UniformBuffer {
     }
 
     public build() {
-        if (!this.bufferData) {
+        if (!this._bufferData) {
             return;
         }
 
@@ -89,14 +108,33 @@ export class UniformBuffer {
         this.bufferGL = GLDevice.gl.createBuffer();
         GLDevice.gl.bindBuffer(GLDevice.gl.UNIFORM_BUFFER, this.bufferGL);
         if (this._dynamic) {
-            GLDevice.gl.bufferData(GLDevice.gl.UNIFORM_BUFFER, this.bufferData, GLDevice.gl.DYNAMIC_DRAW);
+            GLDevice.gl.bufferData(GLDevice.gl.UNIFORM_BUFFER, this._bufferData, GLDevice.gl.DYNAMIC_DRAW);
         } else {
-            GLDevice.gl.bufferData(GLDevice.gl.UNIFORM_BUFFER, this.bufferData, GLDevice.gl.STATIC_DRAW);
+            GLDevice.gl.bufferData(GLDevice.gl.UNIFORM_BUFFER, this._bufferData, GLDevice.gl.STATIC_DRAW);
         }
         GLDevice.gl.bindBuffer(GLDevice.gl.UNIFORM_BUFFER, null);
     }
 
     public update() {
         throw new Error("Not implemented");
+    }
+
+    private alignUniform(size: number) {
+        let alignment = 0;
+        if (size <= 2) {
+            alignment = size;
+        } else {
+            alignment = 4;
+        }
+
+        if ((this._currStartIdx % alignment) !== 0) {
+            var oldPointer = this._currStartIdx;
+            this._currStartIdx += alignment - (this._currStartIdx % alignment);
+            var diff = this._currStartIdx - oldPointer;
+
+            for (let i = 0; i < diff; i++) {
+                this._data.push(0);
+            }
+        }
     }
 }
