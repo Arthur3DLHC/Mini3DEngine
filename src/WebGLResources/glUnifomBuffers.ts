e90d8fe7c77c36bfc034b8f36501eb0bc3ceb7b4
Arthur3DLHC <462747437@qq.com>
2020-04-24 18:08:52 +0800

    add uniform block names

--- src/WebGLResources/glUnifomBuffers.ts
+++ src/WebGLResources/glUnifomBuffers.ts
@@ -2,8 +2,10 @@ import { UniformBuffer } from "./uniformBuffer.js";
 
 export class GLUniformBuffers {
     // TODO: 给 shaderProgram 中的 uniformBlock 分配绑定索引的方法
+    private _uniformBlockNames: {[key:string]: number} = {};
+    
     // TODO: 绑定要使用的 Uniform Buffer 的方法
-    public bindUniformBuffer(buffer: UniformBuffer | null, unifomBlockName: string) {
+    public static bindUniformBuffer(buffer: UniformBuffer | null, unifomBlockName: string) {
         throw new Error("Not implemented");
     }
 }
