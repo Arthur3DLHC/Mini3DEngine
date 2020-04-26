0c4aba8fa68c0ad0695b53ba313799f3d1acfe71
Arthur3DLHC <462747437@qq.com>
2020-04-26 14:28:29 +0800

    single color material

---
+++ src/scene/materials/singleColorMaterial.ts
@@ -0,0 +1,10 @@
+import { Material } from "./material.js";
+
+/**
+ * single color material, for debug or information drawing.
+ */
+export class SingleColorMaterial extends Material {
+    public constructor() {
+        super();
+    }
+}
