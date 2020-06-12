71bbdd3f5b6932d2a748f525eb64d4d82b497c76
Arthur3DLHC <462747437@qq.com>
2020-06-12 18:51:34 +0800

    test ACES tone mapping

--- src/renderer/shaders/default_pbr_fs.glsl.ts
+++ src/renderer/shaders/default_pbr_fs.glsl.ts
@@ -50,6 +50,19 @@ float getOpacity() {
     return u_material.baseColor.a;
 }
 
+// https://blog.csdn.net/z18636930051/article/details/79528753
+// todo: consider to put this to function_postprocess
+vec3 ACESToneMapping(vec3 color, float adapted_lum)
+{
+    const float A = 2.51;
+    const float B = 0.03;
+    const float C = 2.43;
+    const float D = 0.59;
+    const float E = 0.14;
+    color *= adapted_lum;
+    return (color * (A * color + B)) / (color * (C * color + D) + E);
+}
+
 void main(void)
 {
     FinalOutput o = defaultFinalOutput();
@@ -238,10 +251,11 @@ void main(void)
         f_specular += iblSpecular / totalWeight;
     }
     // todo: opacity for transparent surfaces;
-    o.color = vec4(f_diffuse + f_specular + f_emissive, getOpacity());
+    vec3 color = f_diffuse + f_specular + f_emissive;
+    color = ACESToneMapping(color, 1.0);
+    o.color = vec4(color, getOpacity());
 
     // todo: tone mapping? linear space to sRGB space?
-
     outputFinal(o);
 }
 `;
