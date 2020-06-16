e7d2159c46cd02ff3848ddb7f07555282b3c9d37
Arthur3DLHC <462747437@qq.com>
2020-06-16 11:45:28 +0800

    ssao shader

--- src/renderer/shaders/postprocess_ssao_fs.glsl.ts
+++ src/renderer/shaders/postprocess_ssao_fs.glsl.ts
@@ -1,5 +1,6 @@
 /**
  * ssao effect
+ * mostly form three.js  SSAOShader.js
  */
 export default /** glsl */`
 #include <uniforms_view>
@@ -53,7 +54,42 @@ vec3 getViewNormal(vec2 screenPosition) {
 }
 
 void main(void) {
+    float depth = texture(s_sceneDepth, ex_texcoord);
+    float viewZ = getViewZ(depth);
 
+    vec3 viewPosition = getViewPosition(ex_texcoord, depth, viewZ);
+    vec3 viewNormal = getViewNormal(ex_texcoord);
+
+    vec2 noiseScale = u_noiseTexelSize / u_texelSize;   // == texture res / noisetexture res
+    vec3 random = texture(s_noiseTex, ex_texcoord * noiseScale);
+
+	// compute matrix used to reorient a kernel vector
+    vec3 tangent = normalize(random - viewNormal * dot(random, viewNormal));
+    vec3 bitangent = corss(viewNormal, tangent);
+    mat3 kernelMatrix = mat3(tangent, bitangent, viewNormal);
+
+    float occlusion = 0.0;
+
+    for (int i = 0; i < NUM_KERNELS; i++) {
+        vec3 sampleVector = kernelMatrix * u_kernel[i];
+        vec3 samplePoint = viewPosition + (sampleVector * u_radius);
+
+        vec4 samplePointNDC = u_view.matProj * vec4(samplePoint, 1.0);
+        samplePointNDC /= samplePointNDC.w;
+
+        vec2 samplePointUV = samplePointNDC.xy * 0.5 + vec2(0.5);
+
+        float realDepth = getLinearDepth(samplePointUV);    // scene depth value in depth texture
+        float sampleDepth = viewZToOrthoDepth(samplePoint.z, u_view.zRange.x, u_view.zRange.y); // linear depth of sample view Z
+        float delta = sampleDepth - realDepth;
+
+        if (delta > u_minDistance && delta < u_maxDistance) {
+            occlusion += 1.0;
+        }
+    }
+
+    occlusion = clamp(occlusion / float(NUM_KERNELS), 0.0, 1.0);
+    o_color = vec4(vec3(1.0 - occlusion), 1.0);
 }
 
 `;
