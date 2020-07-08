f677b057bbead6501cb2aa62695075545de8c0c8
Arthur3DLHC <462747437@qq.com>
2020-07-08 15:11:16 +0800

    cache and image loader class

---
+++ src/contentLoaders/cache.ts
@@ -0,0 +1,28 @@
+// Adapted from https://github.com/mrdoob/three.js/blob/dev/src/loaders/Cache.js
+
+/**
+ * 
+ */
+export class Cache {
+    // put everything in one map? or put objects of different types in different maps?
+    public static enabled: boolean = false;
+    public static files: Map<string, any> = new Map<string, any>();
+
+    public static add(key: string, file: any) {
+		if ( this.enabled === false ) return;
+        this.files.set(key, file);
+    }
+
+    public static get(key: string): any {
+		if ( this.enabled === false ) return;
+        return this.files.get(key);
+    }
+
+    public static remove(key: string) {
+        this.files.delete(key);
+    }
+
+    public static clear() {
+        this.files.clear();
+    }
+}
--- src/contentLoaders/imageLoader.ts
+++ src/contentLoaders/imageLoader.ts
@@ -1,6 +1,75 @@
+import { LoadingManager } from "./loadingmanager.js";
+import { Cache } from "./cache.js";
+
 /**
  * Image loader
  */
 export class ImageLoader {
-    
+    public constructor(manager: LoadingManager) {
+        this.manager = manager;
+    }
+
+    public path: string | undefined;
+    public crossOrigin: string | undefined;
+    public manager: LoadingManager;
+
+    public load(url: string, 
+        onImgLoad?:(image: HTMLImageElement)=>void,
+        onProgress?:(event: ProgressEvent)=>void,
+        onError?:(event: ErrorEvent)=>void): HTMLImageElement
+    {
+        if (this.path !== undefined) { url = this.path + url; }
+        url = this.manager.resolveURL(url);
+
+        let cached = Cache.get(url);
+        if (cached !== undefined) {
+            this.manager.itemStart(url);
+
+            setTimeout(() => {
+                if (onImgLoad !== undefined) {
+                    onImgLoad(cached);
+                }
+                this.manager.itemEnd(url);
+            }, 0);
+            return cached;
+        }
+
+        const image = document.createElementNS('http://www.w3.org/1999/xhtml', 'img') as HTMLImageElement;
+        const self = this;
+
+        function onImageLoad() {
+            image.removeEventListener('load', onImageLoad, false);
+            image.removeEventListener('error', onImageError, false);
+
+            Cache.add(url, image);
+
+            if (onImgLoad) {
+                onImgLoad(image);
+            }
+            self.manager.itemEnd(url);
+        }
+
+        function onImageError(event: ErrorEvent) {
+            image.removeEventListener('load', onImageLoad, false);
+            image.removeEventListener('error', onImageError, false);
+
+            if(onError) onError(event);
+
+            self.manager.itemError(url);
+            self.manager.itemEnd(url);
+        }
+
+        image.addEventListener('load', onImageLoad, false);
+        image.addEventListener('error', onImageError, false);
+
+        if ( url.substr( 0, 5 ) !== 'data:' ) {
+
+			if ( this.crossOrigin !== undefined ) image.crossOrigin = this.crossOrigin;
+
+        }
+        
+        self.manager.itemStart(url);
+        image.src = url;
+        return image;
+    }
 }
