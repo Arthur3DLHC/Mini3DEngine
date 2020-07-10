df495cd08add123120988bed4bec5f4118ea56d6
Arthur3DLHC <462747437@qq.com>
2020-07-10 16:26:30 +0800

    Hammersley class

---
+++ src/math/Hammersley.ts
@@ -0,0 +1,16 @@
+// https://zhuanlan.zhihu.com/p/20374706
+
+export class Hammersley {
+    public static get(index: number, base: number, numSamples: number): number {
+        throw new Error("Not implemented.");
+    }
+
+    private static radicalInverse(index: number, base: number): number {
+        // while (i > 0) {
+        //     result = result + f * (i % base);
+        //     i = Math.floor(i / base);
+        //     f = f / base;
+        // }
+        throw new Error("Not implemented.");
+    }
+}
