1e74835ef7948bd06948f9a7962a2f99055ba65f
Arthur3DLHC <462747437@qq.com>
2020-08-06 17:33:58 +0800

    update joint matrices

--- src/renderer/shaders/shaderIncludes/function_transforms.glsl.ts
+++ src/renderer/shaders/shaderIncludes/function_transforms.glsl.ts
@@ -7,17 +7,20 @@ export default /** glsl */`
 #ifdef USE_SKINNING
 
         mat4 matSkin = getSkinningMatrix();
-        vLocal = matSkin * vLocal;
+        return matSkin * vLocal;
+        
+#else
+
+        return u_object.matWorld * vLocal;
 
 #endif 
-        return u_object.matWorld * vLocal;
     }
 
     vec4 worldToView(vec4 vWorld)
     {
         return u_view.matView * vWorld;
     }
-    
+
     vec4 viewToProj(vec4 vView)
     {
         return u_view.matProj * vView;
--- src/scene/skinMesh.ts
+++ src/scene/skinMesh.ts
@@ -10,6 +10,14 @@ export class SkinMesh extends Mesh {
     public inverseBindMatrices: mat4[] = [];
 
     public updateJointMatrices(){
-
+        for(let i = 0; i < this.joints.length; i++) {
+            const joint = this.joints[i];
+            let ibm = this.inverseBindMatrices[i];
+            let jointMatrix = this.jointMatrices[i];
+            mat4.product(joint.worldTransform, ibm, jointMatrix);
+            // do not need to multiply with the inv world matrix of parent node,
+            // as https://github.com/KhronosGroup/glTF-Sample-Viewer/blob/master/src/skin.js do,
+            // because we do not multiply the world matrix of parent node in shader.
+        }
     }
 }
