c49b37ba6f3b09b7193503bc84aaec69d8ee110b
Arthur3DLHC <462747437@qq.com>
2020-08-25 13:19:49 +0800

    bounding sphere enlarge

--- src/math/boundingSphere.ts
+++ src/math/boundingSphere.ts
@@ -28,21 +28,31 @@ export class BoundingSphere {
         return result;
     }
 
+    /**
+     * enlarge this bounding sphere to contain another one
+     * @param sphere bounding sphere to contain
+     */
     enlarge(sphere: BoundingSphere) {
         // center may change
         // from this center to new sphere center
         const offset: vec3 = sphere.center.copy()
         offset.subtract(this.center);
         const dist: number = offset.length();
-        if (dist < 0.0001) {    // almost same center, no need to move
-            this.radius = Math.max(this.radius, sphere.radius);
+        const r1 = this.radius;
+        const r2 = sphere.radius;
+        if (dist + r2 <= r1) { //contains, no need to change
             return;
         }
-        let d = dist + this.radius + sphere.radius;
-        d = Math.max(d, Math.max(this.radius, sphere.radius));
+        if (dist + r1 <= r2) {
+            this.radius = r2;
+            sphere.center.copy(this.center);
+            return;
+        }
+        let d = dist + r1 + r2;
+        // d = Math.max(d, Math.max(this.radius, sphere.radius));
         const newRadius = d * 0.5;
         // calculate new center
-        const distToNewCenter: number = sphere.radius + dist - newRadius; // the last is new radius
+        const distToNewCenter: number = r2 + dist - newRadius; // the last is new radius
         const s: number = distToNewCenter / dist;
         this.center.add(offset.scale(s));
         this.radius = newRadius;
