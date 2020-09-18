9fc35918abe4d667111189850d321eead5670335
Arthur3DLHC <462747437@qq.com>
2020-09-18 12:19:27 +0800

    test hdr skybox

--- src/renderer/shaders/skybox_fs.glsl.ts
+++ src/renderer/shaders/skybox_fs.glsl.ts
@@ -7,7 +7,8 @@ precision mediump samplerCube;
 #include <uniforms_object>
 
 uniform samplerCube s_skybox;
-uniform int u_isHDR;            // use RGBE format
+
+#define u_isHDR     u_object.props.w
 
 in vec3 ex_worldDir;
 in vec3 ex_normal;
@@ -17,7 +18,7 @@ in vec2 ex_texcoord;
 
 void main(void) {
     vec4 texColor = texture(s_skybox, ex_worldDir);
-    if (u_isHDR == 1) {
+    if (u_isHDR == 1.0) {
         texColor.rgb *= pow(2.0, texColor.a * 255.0 - 128.0);
         texColor.a = 1.0;
     }
