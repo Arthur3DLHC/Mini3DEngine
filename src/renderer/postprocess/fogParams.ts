3974ec08c9aac5e596c2c37c07355f8c4e9f88b9
Arthur3DLHC <462747437@qq.com>
2020-10-26 11:29:32 +0800

    fog shader

--- src/renderer/postprocess/fogParams.ts
+++ src/renderer/postprocess/fogParams.ts
@@ -1,13 +1,17 @@
+import vec3 from "../../../lib/tsm/vec3.js";
+
 export class FogParams {
     public constructor() {
         this.enable = false;
         this.density = 0.001;
+        this.color = vec3.one.copy()
         this.halfSpace = false;
         this.height = 0;
     }
 
     public enable: boolean;
     public density: number;
+    public color: vec3;
     public halfSpace: boolean;
     public height: number;
 }
---
+++ src/renderer/shaders/postprocess_fog_fs.glsl.ts
@@ -0,0 +1,22 @@
+/**
+ * simple fog shader
+ * exp fog, support height half space
+ */
+export default /** glsl */`
+uniform float u_density;
+uniform vec3 u_color;
+uniform int u_halfSpace;
+uniform float u_height;
+// need the scene depth map 
+#include <samplers_postprocess>
+
+// full screen quad texcoord input
+in vec2 ex_texcoord;
+layout(location = 0) out vec4 o_color;
+
+#include <function_depth>
+
+void main(void) {
+    o_color = vec4(u_color, 0.5);
+}
+`;
