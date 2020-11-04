95e06bf4caacd513b5b35f855dc08c4070f39b41
Arthur3DLHC <462747437@qq.com>
2020-11-04 22:20:11 +0800

    remove unused params

--- src/renderer/postprocess/fxaaParams.ts
+++ src/renderer/postprocess/fxaaParams.ts
@@ -4,7 +4,7 @@
  */
 export class FXAAParams {
     public enable: boolean = true;
-    public subPixel: number = 1.0;
-    public edgeThreshold: number = 0.166;
-    public edgeThresholdMin: number = 0.0833;
+    // public subPixel: number = 1.0;
+    // public edgeThreshold: number = 0.166;
+    // public edgeThresholdMin: number = 0.0833;
 }
