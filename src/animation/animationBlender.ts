631e0b024abb85af11f6da4fcf8bb6c11d1063d5
Arthur3DLHC <462747437@qq.com>
2020-11-07 16:02:41 +0800

    animation blender class

---
+++ src/animation/animationBlender.ts
@@ -0,0 +1,9 @@
+export class AnimationBlender {
+    // todo: hold blend tree nodes
+    public update() {
+        // todo: update node weights
+        // calculate weights for every action (also for channels in animationAction)
+        // apply action channels by weight (animationChannel.apply() has a weight parameter)
+        // normalize quaternions ?
+    }
+}
