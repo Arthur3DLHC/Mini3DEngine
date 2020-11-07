754159275fcd019112b0dc5d3e174125053d000e
Arthur3DLHC <462747437@qq.com>
2020-11-07 14:39:16 +0800

    test import cannon.es

--- src/physics/physicsBehavior.ts
+++ src/physics/physicsBehavior.ts
@@ -1,10 +1,21 @@
 import { Behavior } from "../scene/behavior.js";
+import { Object3D } from "../scene/object3D.js";
+import { PhysicsWorld } from "./physicsWorld.js";
 
 /**
  * base physics behavior
  */
 export class PhysicsBehavior extends Behavior {
+    public constructor(owner: Object3D, physicsWorld: PhysicsWorld) {
+        super(owner);
+        this._world = physicsWorld;
+    }
+
+    public get world() { return this._world; }
+
     public update() {
 
     }
+
+    private _world: PhysicsWorld;
 }
--- src/physics/physicsWorld.ts
+++ src/physics/physicsWorld.ts
@@ -1,3 +1,7 @@
+/// <reference path = "../../tsDefinitions/cannon-es.d.ts" />
+
+import * as Cannon from "cannon-es";
+
 export class PhysicsWorld {
-    
+    world: Cannon.World | null = null;
 }
