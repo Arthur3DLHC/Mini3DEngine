d47058756a538911895e64aca44b3873cf000c53
Arthur3DLHC <462747437@qq.com>
2020-11-10 17:02:57 +0800

    init physics world

--- src/physics/physicsWorld.ts
+++ src/physics/physicsWorld.ts
@@ -2,9 +2,28 @@
 
 import { Clock } from "../scene/clock.js";
 
+export interface PhysicsWorldOptions {
+    useSplitSolver?: boolean;
+    numIterations?: number;
+    tolerance?: number;
+}
+
 export class PhysicsWorld {
-    public constructor() {
+    public constructor(options?: PhysicsWorldOptions) {
         this._world = new CANNON.World();
+        // todo: do some common init works?
+        this._world.broadphase = new CANNON.NaiveBroadphase();
+
+        const solver = new CANNON.GSSolver();
+        if (options) {
+            if (options.numIterations !== undefined) {solver.iterations = options.numIterations;}
+            if (options.tolerance !== undefined) {solver.tolerance = options.tolerance;}
+        }
+        if (options && options.useSplitSolver) {
+            this._world.solver = new CANNON.SplitSolver(solver);
+        } else {
+            this._world.solver = solver;
+        }
     }
 
     public get world(): CANNON.World
