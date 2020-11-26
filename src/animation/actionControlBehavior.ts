e776b10510c91f20c47c54fcfa4f9df127ec66d3
Arthur3DLHC <462747437@qq.com>
2020-11-26 18:31:00 +0800

    comments

--- src/animation/actionControlBehavior.ts
+++ src/animation/actionControlBehavior.ts
@@ -43,7 +43,7 @@ export class ActionControlBehavior extends Behavior {
     }
 
     public fromJSON(jsonData: any, customStateCreation?: (stateDef: any)=> ActionState, customConditionCreation?: (conditionDef: any)=>ActionCondition) {
-        // params
+        // add params first
         this._actionParams.clear();
 
         if (jsonData.actionParams !== undefined) {
