9e2a1b98fa1e4f6557b3823c3caca6fd0d49bf33
Arthur3DLHC <462747437@qq.com>
2020-12-18 12:47:02 +0800

    adjust bloom intensity

--- tests/actionControl/thirdPersonCtrl.ts
+++ tests/actionControl/thirdPersonCtrl.ts
@@ -63,8 +63,9 @@ window.onload = () => {
     const gltfLoader = new GLTFLoader(loadingManager);
 
     const renderer = new ClusteredForwardRenderer();
-    renderer.postprocessor.bloom.intensity = 0.5;
-    renderer.postprocessor.bloom.radius = 0.5;
+    renderer.postprocessor.bloom.intensity = 0.3;
+    renderer.postprocessor.bloom.radius = 0.2;
+    renderer.postprocessor.bloom.threshold = 1.5;
 
     const scene = new Scene();
     const camera = new PerspectiveCamera();
