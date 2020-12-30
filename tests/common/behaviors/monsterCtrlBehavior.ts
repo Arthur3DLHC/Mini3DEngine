0372dbaa4c674e64656ffbd5b74b00c2666474d0
Arthur3DLHC <462747437@qq.com>
2020-12-30 15:47:07 +0800

    monster ctrl cont.

--- tests/common/behaviors/monsterCtrlBehavior.ts
+++ tests/common/behaviors/monsterCtrlBehavior.ts
@@ -63,14 +63,22 @@ export class MonsterCtrlBehavior extends Behavior {
     private _player: Object3D | null = null;
     private _destination: vec3 = new vec3();
 
+    private _destinationDir: vec3 = new vec3();
+    private _distToPlayer: number = 10000;
+
+    /** cur facing dir calculated from cur yaw and pitch */
+    private _facingDir: vec3 = new vec3();
+
     // private _lastThinkTime: number = 0;
     // cur state?
     private _curState: MonsterState = MonsterState.Idle;
+    // private _curAction: number = 0;
     /** recover time left for states like attacking, attacked, down */
     private _recoverTimeLeft: number = 0;
 
     private static _tmpMyPosition: vec3 = new vec3();
     private static _tmpPlayerPosition: vec3 = new vec3();
+    // private static _tmpDir: vec3 = new vec3();
 
     public start() {
 
@@ -82,16 +90,38 @@ export class MonsterCtrlBehavior extends Behavior {
         //     this.think();
         //     this._lastThinkTime = curTime;
         // }
-        let curAction: number = 0;
+
+        // facing dir
+        this._facingDir.setComponents(0, 0, 0);
+        let facingYaw = this.yaw + Math.PI * 0.5;
+        this._facingDir.x = Math.cos(facingYaw);
+        this._facingDir.z = -Math.sin(facingYaw);
+
+        if (this._player !== null) {
+            this.owner.worldTransform.getTranslation(MonsterCtrlBehavior._tmpMyPosition);
+            this._player.worldTransform.getTranslation(MonsterCtrlBehavior._tmpPlayerPosition);
+
+            vec3.direction(MonsterCtrlBehavior._tmpMyPosition, MonsterCtrlBehavior._tmpPlayerPosition, this._destinationDir);
+            this._distToPlayer = vec3.distance(MonsterCtrlBehavior._tmpMyPosition, MonsterCtrlBehavior._tmpPlayerPosition);
+        }
+
         switch(this._curState) {
             case MonsterState.Idle:
                 // set actionCtrl params
-                // if player in sight, move ?
+                // this._curAction = 0;
+                // if player in attack range, attack ?
+                if(this.playerInMeleeAttackRange()) {
+                    this.attack();
+                } else if(this.playerInSight()) {
+                    // if player in sight, move ?
+                    this.moveTo(MonsterCtrlBehavior._tmpPlayerPosition);
+                }
                 break;
             case MonsterState.Moving:
                 // set actionCtrl params
+                // upate destination position
                 // turn toward destination position
-                // move toward destination position
+                // move toward cur facing dir
                 // if approached destination position, idle or attack ?
                 // refers to cur behavior is patrolling or chasing?
                 break;
@@ -102,36 +132,40 @@ export class MonsterCtrlBehavior extends Behavior {
             case MonsterState.Attacked:
                 // set actionCtrl params
                 // recover time left
+                // if recovered (and player in sense range?), move toward player
                 break;
             case MonsterState.Down:
                 // set actionCtrl params
                 break;
         }
-        this._actionCtrl.actionParams.set("curAction", curAction);
+        // this._actionCtrl.actionParams.set("curAction", this._curAction);
     }
 
     public moveTo(dest: vec3) {
         if (this._curState === MonsterState.Idle || this._curState === MonsterState.Moving) {
             dest.copy(this._destination);
             this._curState = MonsterState.Moving;
+            this._actionCtrl.actionParams.set("curAction", MonsterState.Moving);
         }
     }
 
     public attack() {
         // attack toward current orientation ?
         // if facing player and close enough, player take damage ?
-        if (this._curState === MonsterState.Idle || this._curState === MonsterState.Moving) {
+        // if (this._curState === MonsterState.Idle || this._curState === MonsterState.Moving) {
             this._curState = MonsterState.Attacking;
             this._recoverTimeLeft = 1.5;
             // todo: select attack action randomly
-        }
+            this._actionCtrl.actionParams.set("curAction", this._curState * 100 + Math.round(Math.random() * (this.attackingActions - 1)));
+        // }
     }
 
     public onAttacked() {
-        if (this._curState === MonsterState.Idle || this._curState === MonsterState.Moving || this._curState === MonsterState.Attacking) {
+        if (this._curState !== MonsterState.Down) {
             this._curState = MonsterState.Attacked;
+            this._actionCtrl.actionParams.set("curAction", MonsterState.Attacked);
             this._recoverTimeLeft = 1.0;
-            // if hp < 0, down
+            // todo: if hp < 0, down
         }
     }
 
@@ -147,20 +181,42 @@ export class MonsterCtrlBehavior extends Behavior {
         }
 
         // distance
-        this.owner.worldTransform.getTranslation(MonsterCtrlBehavior._tmpMyPosition);
-        this._player.worldTransform.getTranslation(MonsterCtrlBehavior._tmpPlayerPosition);
-
-        if (vec3.squaredDistance(MonsterCtrlBehavior._tmpMyPosition, MonsterCtrlBehavior._tmpPlayerPosition) > this.senseRange * this.senseRange) {
+        if (this._distToPlayer > this.senseRange) {
             return false;
         }
 
         // orientation ? sense player by vision, sound or smell ?
         if (this.senseHalfFOV < Math.PI) {
-            // todo: compare cos and dotproduct ?
-            const cosFOV = Math.cos(this.senseHalfFOV);
+            if (!this.inView(this._destinationDir, this.senseHalfFOV)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    private playerInMeleeAttackRange(): boolean {
+        if (this._player === null) {
+            return false;
         }
 
+        // distance
+        if (this._distToPlayer > this.meleeAttackRange) {
+            return false;
+        }
+
+        // orientation ? sense player by vision, sound or smell ?
+        if (this.meleeAttackHalfFOV < Math.PI) {
+            if (!this.inView(this._destinationDir, this.meleeAttackHalfFOV)) {
+                return false;
+            }
+        }
         return true;
+    }
+
+    private inView(targetDir: vec3, halfFOV: number): boolean {
+        const cosFOV = Math.cos(halfFOV);
+        const cosFacing = vec3.dot(this._facingDir, targetDir);
+        // facing angle < half fov
+        return cosFacing > cosFOV;
     }
 }
