69e5c90e49ce66d22ddfe20c8ee88a34d065a512
Arthur3DLHC <462747437@qq.com>
2021-01-03 16:30:24 +0800

    gltf assets

--- src/contentLoaders/prefabManager.ts
+++ src/contentLoaders/prefabManager.ts
@@ -1,16 +1,35 @@
 import { Object3D } from "../scene/object3D.js";
 import { Node } from "./gltf.js";
+import { GltfAsset } from "./gltfAsset.js";
 
 export class PrefabManager {
     // register prefabs
     // fix me: use object3d or use JSON object as prefab?
-    // need a behavior factory to create behavior by type?
-    public prefabs: Map<string, Object3D> = new Map<string, Object3D>();
+    // need a behavior factory to create behaviors by type?
+    // use interface?
+    public prefabs: Map<string, any> = new Map<string, any>();
+
+    /**
+     * fill this map after all gltf assets loaded?
+     */
+    public gltfAssets: Map<string, GltfAsset> = new Map<string, GltfAsset>();
 
     // create object from prefab
     public createFromPrefab(key: string): Object3D {
         // find the prefab object with key
         // clone the object and it's all behaviors?
+        const prefab = this.prefabs.get(key);
+        if (prefab === undefined) {
+            throw new Error("Prefab not found:" + key);
+        }
+        if (prefab.gltf !== undefined) {
+            // need to load gltf?
+            // or need to get gltf model from already loaded resources?
+        }
+
+        // const ret: Object3D = new Object3D();
+        
+
         throw new Error("Not implemented.");
     }
 
