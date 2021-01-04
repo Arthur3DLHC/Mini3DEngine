fcc0ef577c2bf62ef93fb2759df7f5050d500f1c
Arthur3DLHC <462747437@qq.com>
2021-01-04 18:53:48 +0800

    behavior factories

---
+++ tests/actionControl/actionControlBehaviorFactory.ts
@@ -0,0 +1,13 @@
+import { Behavior, Object3D } from "../../src/mini3DEngine.js";
+import { CommonBehaviorFactory } from "../common/behaviors/commonBehaviorFactory.js";
+import { ThirdPersonShooterBehavior } from "./thirdPersonShooterBehavior.js";
+
+export class ActionControlBehaviorFactory extends CommonBehaviorFactory {
+    createBehavior(behaviorType: string, owner: Object3D, options: any): Behavior {
+        if (behaviorType === "ThirdPersonShooterBehavior") {
+            throw new Error("Not implemented");
+        } else {
+            return super.createBehavior(behaviorType, owner, options);
+        }
+    }
+}
--- tests/common/behaviors/commonBehaviorFactory.ts
+++ tests/common/behaviors/commonBehaviorFactory.ts
@@ -1,9 +1,24 @@
 import { Behavior, BehaviorFactory, Object3D } from "../../../src/mini3DEngine.js";
+import { AutoRotateBehavior } from "./autoRotateBehavior.js";
+import { FirstPersonViewBehavior } from "./firstPersonViewBehavior.js";
+import { MonsterCtrlBehavior } from "./monsterCtrlBehavior.js";
+import { ThirdPersonCtrlBehavior } from "./thirdPersonCtrlBehavior.js";
 
 export class CommonBehaviorFactory implements BehaviorFactory {
-    
+
     createBehavior(behaviorType: string, owner: Object3D, options: any): Behavior {
+        switch(behaviorType) {
+            case "AutoRotateBehavior":
+                break;
+            case "FirstPersonViewBehavior":
+                break;
+            case "LookatBehavior":
+                break;
+            case "MonsterCtrlBehavior":
+                break;
+            case "ThirdPersonCtrlBehavior":
+                break;
+        }
         throw new Error("Method not implemented.");
     }
-
 }
