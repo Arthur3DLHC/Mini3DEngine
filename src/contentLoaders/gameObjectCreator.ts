241d9a7b9978de5b87918762f634dcf3814b9008
Arthur3DLHC <462747437@qq.com>
2021-01-06 15:17:28 +0800

    TPS Game obj creator

--- src/contentLoaders/gameObjectCreator.ts
+++ src/contentLoaders/gameObjectCreator.ts
@@ -1,11 +1,14 @@
+import { PhysicsWorld } from "../physics/physicsWorld.js";
 import { Object3D } from "../scene/object3D.js";
 import { GltfAsset } from "./gltfAsset.js";
 
 export abstract class GameObjectCreator {
     /**
-     * gltf assets for creating the model
+     * gltf assets for creating the models of gameobjects
      */
     public gltfAssets: Map<string, GltfAsset> = new Map<string, GltfAsset>();
+
+    public physicsWorld: PhysicsWorld | null = null;
 
     /**
      * subclass implement this method to create their own game objects according to prefab key
--- tests/scifiTPSGame/scifiGameObjCreator.ts
+++ tests/scifiTPSGame/scifiGameObjCreator.ts
@@ -1,9 +1,21 @@
 import { GameObjectCreator, Object3D } from "../../src/mini3DEngine.js";
 
 export class SciFiGameObjCreator extends GameObjectCreator {
-    
+
     public createGameObject(prefabKey: string, componentProps: any): Object3D {
+        switch(prefabKey) {
+            case "player":
+                return this.createPlayer(componentProps);
+            case "infectedFemale":
+                return this.createInfectedFemale(componentProps);
+        }
+        throw new Error("Unrecogonized prefab: " + prefabKey);
+    }
+    
+    createPlayer(componentProps: any): Object3D {
         throw new Error("Method not implemented.");
     }
-
+    createInfectedFemale(componentProps: any): Object3D {
+        throw new Error("Method not implemented.");
+    }
 }
