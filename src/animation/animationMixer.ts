c81bed49e19673a163ae027d69e6e18066bfa4ff
Arthur3DLHC <462747437@qq.com>
2021-01-09 16:58:48 +0800

    comment spell errors

--- src/animation/animationMixer.ts
+++ src/animation/animationMixer.ts
@@ -3,15 +3,14 @@ import { ObjectPropertiesMixer } from "./objectPropertiesMixer.js";
 
 /**
  * hold the mix agents for animation channel targets
- * mix the animation values here, then apply them to actual target not properties
+ * mix the animation values here, then apply them to actual target node properties
  * this is to prevent the instable mix result of animations
  * (prevent zero sum weights, normalize the sum weights, and so on)
  * 
  * I used to mix the animation on the Object3D's scale, rotate, translation directly,
  * by clear their value to zero first, then add weightd values of every animation
  * but because animations to mix together may not have exactly same target nodes,
- * some target's weight will keep zero or not 1 after the mixing
- * it's necessary
+ * some node target's weight will keep zero or not be 1 after the mixing
  * so, after invesigated the AnimationMixer code of three.js,
  * I decide to add a similar class to do this.
  */
