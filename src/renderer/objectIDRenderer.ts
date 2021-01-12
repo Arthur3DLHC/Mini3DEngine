1a4d5e3016b7eac1c88cf82ea368831d2bed1f37
Arthur3DLHC <462747437@qq.com>
2021-01-12 23:22:38 +0800

    object id renderer class

---
+++ src/renderer/objectIDRenderer.ts
@@ -0,0 +1,14 @@
+/**
+ * this class is for pixel level picking
+ */
+export class ObjectIDRenderer {
+    // query pick?
+    // every pick can register an event hanlder?
+
+    // in the begining of every frame, check if there are any picking queries;
+    // if there are, read back pixels for every pick and check picking result?
+    // clear the picking queries;
+
+    // in the end of every frame, check if there are new picking queries;
+    // if so, render all pickable object IDs to picking FBO.
+}
