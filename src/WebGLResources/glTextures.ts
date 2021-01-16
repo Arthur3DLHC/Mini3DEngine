c9b208d4a36b307851296502d29946a9f1ed3e03
Arthur3DLHC <462747437@qq.com>
2021-01-16 16:36:29 +0800

    8 bit channel ingeter formats

--- src/WebGLResources/glTextures.ts
+++ src/WebGLResources/glTextures.ts
@@ -39,24 +39,32 @@ export class GLTextures {
                 if (type === gl.UNSIGNED_INT) internalFormat = gl.R32UI;
                 if (type === gl.SHORT) internalFormat = gl.R16I;
                 if (type === gl.UNSIGNED_SHORT) internalFormat = gl.R16UI;
+                if (type === gl.BYTE) internalFormat = gl.R8I;
+                if (type === gl.UNSIGNED_BYTE) internalFormat = gl.R8UI;
                 break;
             case gl.RG_INTEGER:
                 if (type === gl.INT) internalFormat = gl.RG32I;
                 if (type === gl.UNSIGNED_INT) internalFormat = gl.RG32UI;
                 if (type === gl.SHORT) internalFormat = gl.RG16I;
                 if (type === gl.UNSIGNED_SHORT) internalFormat = gl.RG16UI;
+                if (type === gl.BYTE) internalFormat = gl.RG8I;
+                if (type === gl.UNSIGNED_BYTE) internalFormat = gl.RG8UI;
                 break;
             case gl.RGB_INTEGER:
                 if (type === gl.INT) internalFormat = gl.RGB32I;
                 if (type === gl.UNSIGNED_INT) internalFormat = gl.RGB32UI;
                 if (type === gl.SHORT) internalFormat = gl.RGB16I;
                 if (type === gl.UNSIGNED_SHORT) internalFormat = gl.RGB16UI;
+                if (type === gl.BYTE) internalFormat = gl.RGB8I;
+                if (type === gl.UNSIGNED_BYTE) internalFormat = gl.RGB8UI;
                 break;
             case gl.RGBA_INTEGER:
                 if (type === gl.INT) internalFormat = gl.RGBA32I;
                 if (type === gl.UNSIGNED_INT) internalFormat = gl.RGBA32UI;
                 if (type === gl.SHORT) internalFormat = gl.RGBA16I;
                 if (type === gl.UNSIGNED_SHORT) internalFormat = gl.RGBA16UI;
+                if (type === gl.BYTE) internalFormat = gl.RGBA8I;
+                if (type === gl.UNSIGNED_BYTE) internalFormat = gl.RGBA8UI;
                 break;
             case gl.DEPTH_COMPONENT:
                 if (type === gl.UNSIGNED_SHORT) {
