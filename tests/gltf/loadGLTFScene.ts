4ee145752a803fbbbd4351d13cc4db8ebb756a4f
Arthur3DLHC <462747437@qq.com>
2021-01-25 11:27:54 +0800

    canvas size

--- tests/gltf/loadGLTFScene.ts
+++ tests/gltf/loadGLTFScene.ts
@@ -16,8 +16,8 @@ window.onload = () => {
         return;
     }
 
-    canvas.width = Math.max(1280, window.innerWidth - 10);
-    canvas.height = Math.max(720, window.innerHeight - 10);
+    // canvas.width = Math.max(1280, window.innerWidth - 10);
+    // canvas.height = Math.max(720, window.innerHeight - 10);
 
     GLDevice.initialize(canvas);
 
@@ -214,7 +214,7 @@ window.onload = () => {
         }
         else if (gltfNode instanceof EnvironmentProbe) {
             const envProbe = gltfNode as EnvironmentProbe;
-            // envProbe.debugDraw = true;
+            envProbe.debugDraw = true;
         }
 
         for (const child of gltfNode.children) {
