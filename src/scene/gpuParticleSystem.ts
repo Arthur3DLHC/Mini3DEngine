8c45d3aadd098382801d888f656a421dcbec5d6e
Arthur3DLHC <462747437@qq.com>
2021-02-03 08:58:32 +0800

    start impl GPU Psys

--- src/scene/gpuParticleSystem.ts
+++ src/scene/gpuParticleSystem.ts
@@ -2,6 +2,7 @@ import { BufferGeometry } from "../geometry/bufferGeometry.js";
 import { RenderList } from "../renderer/renderList.js";
 import { GLDevice } from "../WebGLResources/glDevice.js";
 import { VertexBuffer } from "../WebGLResources/vertexBuffer.js";
+import { VertexBufferArray } from "../WebGLResources/VertexBufferArray.js";
 import { VertexBufferAttribute } from "../WebGLResources/vertexBufferAttribute.js";
 import { Object3D } from "./object3D.js";
 
@@ -14,13 +15,30 @@ import { Object3D } from "./object3D.js";
 export class GPUParticleSystem extends Object3D {
     public constructor(maxParticleCount: number) {
         super();
+        // use interleaved vertex buffers?
+
+        const data: number[] = [];
+
+        for (let i = 0; i < maxParticleCount; i++) {
+            // position
+            // direction or [billboard rotation angle, rotate speed]?
+            // sideDirection
+            // age
+            // life
+            // seed
+            // size
+            // color
+            // frameIndex: texture animation frame index
+            // noiseTexcoord
+        }
+        
+        const gl = GLDevice.gl;
 
         // todo: create vertex buffers
+        // can use STATIC_DRAW (in babylon.js)
         // this._vertexBuffer = new VertexBuffer(GLDevice.gl.DYNAMIC_DRAW);
-
-        // fix me: format?
-
-        // init data?
+        this._vertexBuffers.push(new VertexBuffer(gl.STATIC_DRAW));
+        this._vertexBuffers.push(new VertexBuffer(gl.STATIC_DRAW));
     }
 
     public destroy() {
@@ -39,9 +57,14 @@ export class GPUParticleSystem extends Object3D {
     private _attributes: VertexBufferAttribute[][] = [];
 
     // todo: use VAOs?
-    // ref: https://gpfault.net/posts/webgl2-particles.txt.html
+    // ref:
+    // https://gpfault.net/posts/webgl2-particles.txt.html
+    // https://github.com/WebGLSamples/WebGL2Samples/blob/master/samples/transform_feedback_instanced.html
+    // https://github.com/BabylonJS/Babylon.js/blob/master/src/Particles/gpuParticleSystem.ts
+
     // use 4 VAOs, 2 for update, 2 for render
-    
+    private _updateVAO: VertexBufferArray[] = [];
+    private _renderVAO: VertexBufferArray[] = [];
 
     // fix me: how to present material and shader program?
 
@@ -52,6 +75,9 @@ export class GPUParticleSystem extends Object3D {
     // textures? support texture animation frames?
 
     // todo: psys properties
+
+    // isBillboard?
+    // has texture animation?
 
     // note: when update, use buffer A as source,
     // and also use buffer A as source when render;
