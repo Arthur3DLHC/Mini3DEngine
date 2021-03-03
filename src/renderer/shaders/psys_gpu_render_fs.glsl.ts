62e6fa0a96939e513706f7eab3d6bff5c7a7b3da
Arthur3DLHC <462747437@qq.com>
2021-03-03 15:14:00 +0800

    multi row anim sheet fs

--- src/renderer/shaders/psys_gpu_render_fs.glsl.ts
+++ src/renderer/shaders/psys_gpu_render_fs.glsl.ts
@@ -7,7 +7,8 @@ export default /** glsl */`
 
 // uniforms
 uniform int u_softParticle;
-uniform float u_texAnimFrames;
+uniform vec3 u_texAnimSheetInfo; // xy: uv scale z: num frames per row
+
 
 // samplers
 uniform sampler2D s_sceneDepth;
@@ -33,10 +34,11 @@ void main(void)
     // o.color = vec4(1.0);
     o.color = ex_color;
 
-    // todo: sample texture and texture animation
-    if(u_texAnimFrames > 0.0) {
+    // sample texture and texture animation
+    if(u_texAnimSheetInfo.z > 0.0) {
         vec4 texcolor = texture(s_texture, ex_texcoord0);
-        if(u_texAnimFrames > 1.0) {
+        // xy is cell size, if they < 1, there are multiple cells in anim sheet.
+        if(u_texAnimSheetInfo.x < 1.0 || u_texAnimSheetInfo.y < 1.0) {
             vec4 nextFrameColor = texture(s_texture, ex_texcoord1);
             texcolor = mix(texcolor, nextFrameColor, ex_texMixAmount);
         }
