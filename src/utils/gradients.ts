6998b5dd8f71fb9c021d69b07c01c41500690827
Arthur3DLHC <462747437@qq.com>
2021-03-04 18:50:42 +0800

    gradients interface

---
+++ src/utils/gradients.ts
@@ -0,0 +1,8 @@
+// take from https://github.com/BabylonJS/Babylon.js/blob/master/src/Misc/gradients.ts,
+// with some modifications.
+
+/** Interface used by value gradients */
+export interface IValueGradient {
+    /** gradient value between 0 and 1 */
+    gradient: number;
+}
